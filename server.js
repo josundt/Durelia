c2b804b0cd5ef54444813d7978c97546efc76d4e
Jørn Andre Sundt <jorn.andre.sundt@jasmin.no>
2016-05-29 18:30:18 +0200

    Added express server to simplify running sample application

---
+++ server.js
@@ -0,0 +1,9 @@
+let express = require("express");
+let app = express();
+app.use(express.static("./"));
+let server = app.listen(8080, () => {
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log(`Express web server started; -listening on port ${port} (host: ${host})`);
+});
